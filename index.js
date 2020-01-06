1c65b79f69a7a4a7f89b5b0f5ecd1803a3f9a95a
Messiahmason1997 <messiahmason1997@gmail.com>
2020-01-05 23:05:59 -0500

    intial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function findLeapYear() {
+	var x = +document.getElementById("str").value;
+	var y = +document.getElementById("end").value;
+	var yearRange = [];
+
+	for (var i = x; i <= y; i++) {
+		yearRange.push(i);
+	}
+
+	var leapYears = [];
+	yearRange.forEach(function(item, index) {
+		if (item % 4 === 0) {
+			leapYears.push(item);
+		}
+	});
+
+	console.log(leapYears);
+}
